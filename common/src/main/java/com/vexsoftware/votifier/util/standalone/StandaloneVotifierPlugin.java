826609db30838707034891dd6e219f954a0cf137
Joseph Hirschfeld <joe@ibj.io>
2016-08-15 19:27:11 -0400

    Update nuvotifer to 2.3.3

--- common/src/main/java/com/vexsoftware/votifier/util/standalone/StandaloneVotifierPlugin.java
+++ common/src/main/java/com/vexsoftware/votifier/util/standalone/StandaloneVotifierPlugin.java
@@ -74,7 +74,7 @@ public class StandaloneVotifierPlugin implements VotifierPlugin, VoteHandler {
 
     @Override
     public String getVersion() {
-        return "2.3.2.2";
+        return "2.3.3";
     }
 
     @Override
