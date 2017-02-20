f8019debd0862f97de6f2bc377957532ab93a43b
Tux <write@imaginarycode.com>
2017-02-19 21:48:59 -0500

    Releasing 2.3.4

--- sponge/src/main/java/com/vexsoftware/votifier/sponge/VotifierPlugin.java
+++ sponge/src/main/java/com/vexsoftware/votifier/sponge/VotifierPlugin.java
@@ -53,7 +53,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-@Plugin(id = "nuvotifier", name = "NuVotifier", version = "2.3.3")
+@Plugin(id = "nuvotifier", name = "NuVotifier", version = "2.3.4")
 public class VotifierPlugin implements VoteHandler, com.vexsoftware.votifier.VotifierPlugin, ForwardedVoteListener {
     private Logger logger;
     @Inject
