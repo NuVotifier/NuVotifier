a2f485e3eecbcf54577fcece300694fcd6650cb2
Andrew Steinborn <git@steinborn.me>
2018-08-31 13:28:37 -0400

    Independent Votifier server bootstrap

--- common/src/main/java/com/vexsoftware/votifier/platform/VotifierPlugin.java
+++ common/src/main/java/com/vexsoftware/votifier/platform/VotifierPlugin.java
@@ -1,5 +1,6 @@
 package com.vexsoftware.votifier.platform;
 
+import com.vexsoftware.votifier.VoteHandler;
 import com.vexsoftware.votifier.platform.scheduler.VotifierScheduler;
 import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
@@ -7,9 +8,8 @@ import org.slf4j.Logger;
 import java.security.Key;
 import java.security.KeyPair;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
-public interface VotifierPlugin {
+public interface VotifierPlugin extends VoteHandler {
     AttributeKey<VotifierPlugin> KEY = AttributeKey.valueOf("votifier_plugin");
 
     Map<String, Key> getTokens();
