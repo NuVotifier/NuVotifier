f5776dd1cba38a64fe762e83b0e378cd587728bf
Joseph Hirschfeld <joe@ibj.io>
2019-01-03 14:36:03 -0500

    Add missing file

--- sponge/src/main/java/com/vexsoftware/votifier/sponge/VotifierPlugin.java
+++ sponge/src/main/java/com/vexsoftware/votifier/sponge/VotifierPlugin.java
@@ -12,6 +12,8 @@ import com.vexsoftware.votifier.platform.scheduler.VotifierScheduler;
 import com.vexsoftware.votifier.sponge.config.ConfigLoader;
 import com.vexsoftware.votifier.sponge.event.VotifierEvent;
 import com.vexsoftware.votifier.sponge.forwarding.SpongePluginMessagingForwardingSink;
+import com.vexsoftware.votifier.support.forwarding.ForwardedVoteListener;
+import com.vexsoftware.votifier.support.forwarding.ForwardingVoteSink;
 import com.vexsoftware.votifier.util.KeyCreator;
 import io.netty.channel.Channel;
 import org.slf4j.Logger;
