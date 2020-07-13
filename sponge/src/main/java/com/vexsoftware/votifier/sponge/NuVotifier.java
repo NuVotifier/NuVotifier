ad9e6f5fe859cd18418347f6d6b6bab5449fde10
Andrew Steinborn <git@steinborn.me>
2020-07-13 13:22:24 -0400

    Do proper version replacement in Sponge/Velocity plugins using blossom

--- sponge/src/main/java/com/vexsoftware/votifier/sponge/NuVotifier.java
+++ sponge/src/main/java/com/vexsoftware/votifier/sponge/NuVotifier.java
@@ -36,7 +36,7 @@ import java.security.KeyPair;
 import java.util.HashMap;
 import java.util.Map;
 
-@Plugin(id = "nuvotifier", name = "NuVotifier", version = "3.0.0-SNAPSHOT", authors = "Ichbinjoe",
+@Plugin(id = "nuvotifier", name = "NuVotifier", version = "@version@", authors = "Ichbinjoe",
         description = "Safe, smart, and secure Votifier server plugin")
 public class NuVotifier implements VoteHandler, VotifierPlugin, ForwardedVoteListener {
 
--- velocity/src/main/java/com/vexsoftware/votifier/velocity/VotifierPlugin.java
+++ velocity/src/main/java/com/vexsoftware/votifier/velocity/VotifierPlugin.java
@@ -50,7 +50,7 @@ import java.security.KeyPair;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@Plugin(id = "nuvotifier", name = "NuVotifier", version = "3.0.0-SNAPSHOT", authors = "Ichbinjoe",
+@Plugin(id = "nuvotifier", name = "NuVotifier", version = "@version@", authors = "Ichbinjoe",
         description = "Safe, smart, and secure Votifier server plugin")
 public class VotifierPlugin implements VoteHandler, ProxyVotifierPlugin {
 
