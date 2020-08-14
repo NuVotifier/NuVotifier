c996e36b97e705324b1c9bf76dcca62fdbc85d62
Andrew Steinborn <git@steinborn.me>
2020-08-14 14:36:01 -0400

    Don't re-register the raw data channel if it's already registered. Fixes #206

--- sponge/src/main/java/com/vexsoftware/votifier/sponge/forwarding/SpongePluginMessagingForwardingSink.java
+++ sponge/src/main/java/com/vexsoftware/votifier/sponge/forwarding/SpongePluginMessagingForwardingSink.java
@@ -10,17 +10,30 @@ import org.spongepowered.api.network.ChannelBuf;
 import org.spongepowered.api.network.RawDataListener;
 import org.spongepowered.api.network.RemoteConnection;
 
-public class SpongePluginMessagingForwardingSink extends AbstractPluginMessagingForwardingSink implements RawDataListener {
+import java.util.Optional;
 
-    public SpongePluginMessagingForwardingSink(NuVotifier p, String channel, ForwardedVoteListener listener) {
-        super(listener);
-        this.channelBinding = Sponge.getChannelRegistrar().createRawChannel(p, channel);
-        this.channelBinding.addListener(Platform.Type.SERVER, this);
-        this.p = p;
-    }
+public class SpongePluginMessagingForwardingSink extends AbstractPluginMessagingForwardingSink implements RawDataListener {
 
     private final NuVotifier p;
     private final ChannelBinding.RawDataChannel channelBinding;
+
+    public SpongePluginMessagingForwardingSink(NuVotifier p, String channel, ForwardedVoteListener listener) {
+        super(listener);
+
+        Optional<ChannelBinding> binding = Sponge.getChannelRegistrar().getChannel(channel);
+        if (binding.isPresent()) {
+            if (binding.get() instanceof ChannelBinding.RawDataChannel) {
+                this.channelBinding = (ChannelBinding.RawDataChannel) binding.get();
+            } else {
+                throw new IllegalStateException("Found an indexed channel - this is a problem.");
+            }
+        } else {
+            this.channelBinding = Sponge.getChannelRegistrar().createRawChannel(p, channel);
+        }
+
+        this.channelBinding.addListener(Platform.Type.SERVER, this);
+        this.p = p;
+    }
 
     @Override
     public void halt() {
