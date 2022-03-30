41eb6327e62ced062ec089af7d737b720cb8262f
JOO200 <github@joo200.de>
2022-03-30 09:47:46 +0200

    Cleanup tests

--- common/src/test/java/com/vexsoftware/votifier/util/ArgsToVoteTest.java
+++ common/src/test/java/com/vexsoftware/votifier/util/ArgsToVoteTest.java
@@ -1,20 +1,10 @@
 package com.vexsoftware.votifier.util;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
 import com.vexsoftware.votifier.model.Vote;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.lang.reflect.Method;
-import java.util.Locale;
-import java.util.Set;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ArgsToVoteTest {
     @Test
@@ -40,16 +30,19 @@ public class ArgsToVoteTest {
     @Test
     void handleServiceNameGetter() {
         Vote vote = ArgsToVote.parse(new String[] { "username=abc123", "servicename=dummy" });
+        assertEquals("abc123", vote.getUsername());
         assertEquals("dummy", vote.getServiceName());
     }
     @Test
     void handleAddressGetter() {
         Vote vote = ArgsToVote.parse(new String[] { "username=abc123", "address=dummy" });
+        assertEquals("abc123", vote.getUsername());
         assertEquals("dummy", vote.getAddress());
     }
     @Test
     void handleTimestampGetter() {
         Vote vote = ArgsToVote.parse(new String[] { "username=abc123", "timestamp=dummy" });
+        assertEquals("abc123", vote.getUsername());
         assertEquals("dummy", vote.getTimeStamp());
     }
 
@@ -57,9 +50,5 @@ public class ArgsToVoteTest {
     void handlesUsernameKeywordArgument() {
         Vote vote = ArgsToVote.parse(new String[] { "username=dummy_user" });
         assertEquals("dummy_user", vote.getUsername());
-    }
-
-    private static Stream<Set<String>> powerSetArgs() {
-        return Sets.powerSet(ImmutableSet.of("servicename", "address", "timestamp")).stream();
     }
 }
